d78093ae1cd533a5770e1a731d03161e1ee4c824
Adam King <adam81king@gmail.com>
2017-07-10 11:02:20 -0400

    Added several methods

--- MethodsPractice/Program.cs
+++ MethodsPractice/Program.cs
@@ -15,13 +15,25 @@ namespace MethodsPractice
             //Methods are ALWAYS part of a class. Methods are ALWAYS children of classes.
             //This means a method will NEVER be created inside another method or member of the class. However...
             //...methods are frequently CALLED inside of another class or method.
-            Console.WriteLine("Hello. Please enter the first number to be added.");
-            int numberOne = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter the second number to be added.");
-            int numberTwo = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Hello. Please enter the first number to be added.");
+            //int numberOne = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Please enter the second number to be added.");
+            //int numberTwo = int.Parse(Console.ReadLine());
 
-            int answer = Add(numberOne, numberOne);
-            Console.WriteLine(answer);
+            //int answer = Add(numberOne, numberTwo);
+            //Console.WriteLine(answer);
+
+            //RobotWarning("Will Robinson");
+
+            //Console.WriteLine("Please enter a name.");
+            //RobotWarning(Console.ReadLine());
+
+            string myBirthMonth = "September";
+            string myFriendBirthMonth = "December";
+            string myVehicle = Vehicle(myBirthMonth);
+            string myFriendVehicle = Vehicle(myFriendBirthMonth);
+
+            Console.WriteLine("My future vehicle is {0} and Jordan's future vehicle is {1}.", myVehicle, myFriendVehicle);
         }
         //Method header
         //Access Modifier - Return Type - Method Name (in Pascal Case) - Parentheses (sometimes with parameters)
@@ -34,6 +46,28 @@ namespace MethodsPractice
             //The "return" keyword takes whatever value is determined by using this method and sends that value...
             //...back to where the method is called.
         }
-        //If you have a method that does NOT have a return value, you would use the keyword "void."
+        //If you have a method that does NOT have a return value, you would use the keyword "void." When we have a...
+        //"void" return type we DON'T need to use the keyword "return."
+
+        public static void RobotWarning(string name)
+        {
+            Console.WriteLine("Danger, " + name + "!!!");
+        }
+
+
+        public static string Vehicle(string birthMonth)
+        {
+            string vehicleFortune;
+            if (birthMonth.ToUpper() == "SEPTEMBER" || birthMonth.ToUpper() == "OCTOBER")
+            {
+                vehicleFortune = "Hoverboard";
+            }
+            else
+            {
+                vehicleFortune = "SUV";
+            }
+            return vehicleFortune;
+        }
+
     }
 }
